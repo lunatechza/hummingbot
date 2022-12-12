d4fced952eae43c27a024238dd64edf386a5ecae
Rynomster <rmathee@gmail.com>
2022-12-12 08:59:02 +0200

    Initial commit - structure

---
+++ hummingbot/connector/exchange/chainex/chainex_constants.py
@@ -0,0 +1,6 @@
+REST_URL = "https://api.chainex.io/"
+
+# Public API endpoints
+MARKEET_SUMMARY_PATH_URL = "market/summary/"
+SERVER_TIME_PATH_URL = "/timestamp"
+PING_PATH_URL = SERVER_TIME_PATH_URL
