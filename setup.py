ec4318c7090ad5bd7ea9cc275ac8481541dffcaa
david-hummingbot <85695272+david-hummingbot@users.noreply.github.com>
2026-01-07 19:25:21 +0800

    remove whitespaces

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
+import fnmatch
 import os
 import subprocess
 import sys
-import fnmatch
 
 import numpy as np
+from Cython.Build import cythonize
 from setuptools import find_packages, setup
 from setuptools.command.build_ext import build_ext
-from Cython.Build import cythonize
 
 is_posix = (os.name == "posix")
+
 
 # Avoid a gcc warning below:
 # cc1plus: warning: command line option ???-Wstrict-prototypes??? is valid
@@ -28,7 +29,10 @@ def main():
         "hummingbot.connector.gateway.clob_spot.data_sources.injective",
         "hummingbot.connector.gateway.clob_perp.data_sources.injective_perpetual"
     ]
-    packages = [pkg for pkg in all_packages if not any(fnmatch.fnmatch(pkg, pattern) for pattern in excluded_paths)]
+    packages = [
+        pkg for pkg in all_packages
+        if not any(fnmatch.fnmatch(pkg, pattern) for pattern in excluded_paths)
+    ]
     package_data = {
         "hummingbot": [
             "core/cpp/*",
@@ -102,7 +106,7 @@ def main():
         "language": "c++",
         "language_level": 3,
     }
-    
+
     if is_posix:
         cython_kwargs["nthreads"] = cpu_count
 
@@ -128,32 +132,37 @@ def main():
         sys.argv.append(f"--parallel={cpu_count}")
 
     # --- 3. Generate Extensions & Manually Apply Flags ---
-    extensions = cythonize(cython_sources, compiler_directives=compiler_directives, **cython_kwargs)
-    
+    extensions = cythonize(
+        cython_sources,
+        compiler_directives=compiler_directives,
+        **cython_kwargs
+    )
+
     for ext in extensions:
         ext.extra_compile_args = extra_compile_args
         ext.extra_link_args = extra_link_args
 
     # --- 4. Pass the modified extensions to setup ---
-    setup(name="hummingbot",
-          version=version,
-          description="Hummingbot",
-          url="https://github.com/hummingbot/hummingbot",
-          author="Hummingbot Foundation",
-          author_email="dev@hummingbot.org",
-          license="Apache 2.0",
-          packages=packages,
-          package_data=package_data,
-          install_requires=install_requires,
-          ext_modules=extensions,  # <--- Use the list we modified
-          include_dirs=[
-              np.get_include()
-          ],
-          scripts=[
-              "bin/hummingbot_quickstart.py"
-          ],
-          cmdclass={"build_ext": BuildExt},
-          )
+    setup(
+        name="hummingbot",
+        version=version,
+        description="Hummingbot",
+        url="https://github.com/hummingbot/hummingbot",
+        author="Hummingbot Foundation",
+        author_email="dev@hummingbot.org",
+        license="Apache 2.0",
+        packages=packages,
+        package_data=package_data,
+        install_requires=install_requires,
+        ext_modules=extensions,  # <--- Use the list we modified
+        include_dirs=[
+            np.get_include()
+        ],
+        scripts=[
+            "bin/hummingbot_quickstart.py"
+        ],
+        cmdclass={"build_ext": BuildExt},
+    )
 
 
 if __name__ == "__main__":
